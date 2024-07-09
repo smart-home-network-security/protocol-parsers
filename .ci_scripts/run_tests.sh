95914be8112333a4c0d78f09e94be38b134257c6
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-07-09 15:06:21 +0200

    CI: added valgrind & cppcheck scripts

---
+++ .ci_scripts/run_tests.sh
@@ -0,0 +1,20 @@
+EXITCODE=0
+VALGRIND_SUPP="$GITHUB_WORKSPACE/.ci_scripts/valgrind.supp"
+
+for file in $GITHUB_WORKSPACE/bin/test/*
+do
+    if [[ $# -eq 1 && $1 == valgrind ]]
+    then
+        valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --suppressions="$VALGRIND_SUPP" --error-exitcode=1 "$file"
+    else
+        "$file"
+    fi
+
+    # If the exit code is not 0, set EXITCODE to 1
+    if [[ $? -ne 0 ]]
+    then
+        EXITCODE=1
+    fi
+done
+
+exit $EXITCODE
